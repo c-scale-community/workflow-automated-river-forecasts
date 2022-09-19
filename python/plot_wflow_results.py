abac260fa05633aa9f1d0a33e9839bab280bac92
JoostBuitink <44062204+JoostBuitink@users.noreply.github.com>
2022-09-19 11:44:03 +0200

    Improve date retrieval, update plotting

--- python/plot_wflow_results.py
+++ python/plot_wflow_results.py
@@ -8,20 +8,22 @@ Created on Thu Jan 20 18:09:23 2022
 import click
 from datetime import date
 from dateutil.parser import parse
+from dateutil.relativedelta import relativedelta
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
 
 
 @click.command()
-@click.option('--output_dir', required=True, type=str, 
-              default = "../../Data/model_output", 
+@click.option('--output_dir', required=True, type=str,
+              default = "../../Data/model_output",
               help='Directory path where the model output resides, expects naming convention in' +
               'directory names within.',)
-@click.option('--figure_out_dir', required=True, type=str, 
-              default = "../../Public", 
+@click.option('--figure_out_dir', required=True, type=str,
+              default = "../../Public",
               help='Directory path where the resulting figure will be saved.',)
-@click.option('--filename_figure', required=True, type=str, 
+@click.option('--filename_figure', required=True, type=str,
               default = "discharge_ts.png",
               help='Filename of the resulting figure.',)
 
@@ -52,56 +54,84 @@ def plot_discharge_ts(output_dir, figure_out_dir, filename_figure, num_ensembles
         current_date = date.today()
     else:
         current_date = parse(re.sub(r"_", r"-", start_date))
-    
+
+    previous_date = current_date - relativedelta(months=1)
+
     current_month = current_date.month
     current_year = current_date.year
-    # Set correct previous month and year values for ERA5 data download
-    if current_month != 1:
-        prev_month = current_month - 1
-        prev_year = current_year
-    else:
-        prev_month = 12
-        prev_year = current_year - 1
-    
+    prev_month = previous_date.month
+    prev_year = previous_date.year
+
     def month_to_str(month):
         strmonth = str(month)
         if len(strmonth) < 2:
             return "0" + strmonth
         else:
             return strmonth
-    
+
     path_to_era_output = f"{output_dir}/run_ERA5_{prev_year}_{month_to_str(prev_month)}/output.csv"
     era_result = pd.read_csv(path_to_era_output, index_col=0, parse_dates=True)[col_extract]
 
-    # Loop through the different ensemles    
+    # Loop through the different ensemles
     for ens_idx in range(num_ensembles):
         path_to_seas_output = f"{output_dir}/run_SEAS5_ens{ens_idx}_{current_year}_{month_to_str(current_month)}/output.csv"
-              
+
         # Loop through SEAS ensembles, create new dataframe if it is the first ensemble
         if ens_idx == 0:
             seas_result = pd.read_csv(path_to_seas_output, index_col=0, parse_dates=True)[col_extract]
             seas_result = pd.DataFrame(seas_result)
             seas_result.columns = [ens_idx]
-        # Append to existing dataframe    
+        # Append to existing dataframe
         else:
             seas_result[ens_idx] = pd.read_csv(path_to_seas_output, index_col=0, parse_dates=True)[col_extract]
-    
+
     # Create figure
     fig = plt.figure("Plot_discharge", clear=True, tight_layout=True, figsize=(8,5))
     ax = fig.subplots(1)
-    
+
     # Extract date information
     xdate_era = era_result.index.to_pydatetime()
     xdate_seas = seas_result.index.to_pydatetime()
-    
+
     ax.plot(xdate_era, era_result, color="black")
     for ens_idx in range(num_ensembles):
         ax.plot(xdate_seas, seas_result[ens_idx], c="black", alpha=0.5)
-    
+
     ax.set_ylabel("Discharge [m$^3$ s$^{-1}$]")
-    
+
+    fig.savefig(f"{figure_out_dir}/{filename_figure}_orig", dpi=300)
+
+
+    # Calculate quantiles for SEAS5 results
+    quantiles = np.arange(0.1, 1.0, 0.1)
+    seas_quan = seas_result.quantile(quantiles, axis=1).T
+    seas_mean = seas_result.mean(axis=1)
+    # import pdb; pdb.set_trace()
+    colors = ['#8c510a','#bf812d','#dfc27d','#f6e8c3','#c7eae5','#80cdc1','#35978f','#01665e']
+
+    # Create figure
+    fig = plt.figure("Plot_discharge_quantile", clear=True, tight_layout=True, figsize=(8,5))
+    ax = fig.subplots(1)
+
+    # Extract date information
+    xdate_era = era_result.index.to_pydatetime()
+    xdate_seas = seas_result.index.to_pydatetime()
+
+    ax.plot(xdate_era, era_result, color="black")
+    # Plot quantiles
+    for idx, q in enumerate(quantiles[:-1]):
+        ax.fill_between(
+            x=xdate_seas,
+            y1=seas_quan.iloc[:,idx],
+            y2=seas_quan.iloc[:,idx+1],
+            color=colors[idx])
+    # Plot mean
+    ax.plot(xdate_seas, seas_mean, ls="--", c="black")
+
+    ax.set_ylabel("Discharge [m$^3$ s$^{-1}$]")
+
     fig.savefig(f"{figure_out_dir}/{filename_figure}", dpi=300)
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     plot_discharge_ts()
