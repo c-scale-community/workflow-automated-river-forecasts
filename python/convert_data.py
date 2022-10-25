1669ba31e3f20b0e8b2e2c5ab8e8a0b21e5032c8
JoostBuitink <44062204+JoostBuitink@users.noreply.github.com>
2022-10-25 18:25:47 +0200

    fix unit in era5 conversion

--- python/convert_data.py
+++ python/convert_data.py
@@ -117,7 +117,7 @@ def convert_era5(filename, dem_model, dem_forcing, output_dir,
     t2m = era.t2m - 273.15 # K to C
     tp = era.tp * 1000 * 24 # m/h to mm/day
     msl = era.msl * 0.01 # Pa to hPa
-    ssrd = era.ssrd / 86400 # J/m2 to W/m2
+    ssrd = era.ssrd / 3600 # J/m2 to W/m2
 
     # Apply lapse rate to temperature
     t_add_sea_level = temp_correction(dem_forcing)
