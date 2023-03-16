efa94fa930e4c6fb07a78f40bcb1a90b39aebe4f
JoostBuitink <44062204+JoostBuitink@users.noreply.github.com>
2023-03-16 16:50:13 +0100

    improve setup script

--- scripts/setup_directories.sh
+++ scripts/setup_directories.sh
@@ -1,4 +1,4 @@
-
+# Set path to project directory
 project_home=/project/lsda
 
 mkdir -p -m775 $project_home/{Data/{forcing/{converted,downloaded},model_input,model_output,logs},Public,Software,Share/home}
@@ -11,14 +11,14 @@ git clone https://github.com/c-scale-community/use-case-high-res-land-surface-dr
 cp -a $project_home/Software/GitHub/. $project_home/Software
 
 # Replace project home in scripts with real project home
-# sed -i "s|^project_home=to_be_modified|project_home=$project_home|" "$project_home/Software/scripts/prepare.sh"
-# sed -i "s|^project_home=to_be_modified|project_home=$project_home|" "$project_home/Software/scripts/wflow_catchup.sh"
-# sed -i "s|^project_home=to_be_modified|project_home=$project_home|" "$project_home/Software/scripts/wflow_batch.sh"
-# sed -i "s|^project_home=to_be_modified|project_home=$project_home|" "$project_home/Software/scripts/plotting.sh"
 sed -i "s|^project_home=to_be_modified|project_home=$project_home|" "$project_home/Software/scripts/workflow.sh"
 
 ### ADD WFLOW IMAGE
+# singularity pull $project_home/Software/images/wflow_063.sif docker://deltares/wflow:latest
 
 ### BUILD PYTHON IMAGE
+# singularity build $project_home/Software/images/hrlsa_j.sif $project_home/Software/images/hrlsa.def
 
 ### ADD CDSKEY FILE IN $project_home/Share/home/.cdsapirc
+
+### COPY wflow files
